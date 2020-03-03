e7c3632d3adee770d6cc4a21778cfa879a72b42b
Fisayo <aikomofisayo@gmail.com>
2020-03-04 00:13:16 +0100

    Fire up!

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import logo from "./logo.svg";
+import "./App.css";
+// import 'font-awesome/css/font-awesome.min.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from './components/Navbar';
+import Details from './components/Details';
+import Cart from './components/Cart/Cart';
+import Default from './components/Default';
+import ProductList from './components/ProductList';
+import Modal from './components/Modal'
+import {Switch, Route} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  state = {  }
+  render() { 
+    return (
+      <>
+        <Navbar />
+        <Switch>
+          <Route exact path='/' component={ProductList} />
+          <Route path='/details' component={Details} />
+          <Route path='/cart' component={Cart} />
+          <Route component={Default} />
+        </Switch>
+        <Modal />
+      </>
+    );
+  }
 }
+ 
 
 export default App;
